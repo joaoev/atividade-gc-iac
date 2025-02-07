d1727cb3ad65c8a8c5ec097ed0ffc19a933d5ee9
joaoev <joaoalvesxp@alu.ufc.br>
2025-02-07 20:54:33 -0300

    feat: criando terraform principal

---
+++ main.tf
@@ -0,0 +1,48 @@
+variable "digitalocean_api_token" {}
+
+provider "digitalocean" {
+  token = var.digitalocean_api_token
+}
+
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-20-04-x64"
+  name   = "flask-droplet"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+
+  user_data = <<-EOT
+    #!/bin/bash
+    apt update -y && apt upgrade -y
+    apt install -y curl
+
+    # Baixar arquivos do repositório
+    curl -o /tmp/app.py https://raw.githubusercontent.com/joaoev/atividade-gc-iac/main/app.py
+    curl -o /tmp/setup.sh https://raw.githubusercontent.com/joaoev/atividade-gc-iac/main/setup.sh
+
+    # Dar permissão e executar setup
+    chmod +x /tmp/setup.sh
+    /tmp/setup.sh
+  EOT
+
+  provisioner "local-exec" {
+    command = "echo Droplet created with IP: ${self.ipv4_address}"
+  }
+}
+
+resource "digitalocean_firewall" "web_firewall" {
+  name = "flask-firewall"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
